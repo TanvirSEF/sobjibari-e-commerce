817066d8ffc4281bf1a3d88020b50166122e4c0b
Tanvir Hasan <tanvirmern@gmail.com>
2024-10-16 11:37:57 +0600

    front

--- src/app/(front-end)/page.js
+++ src/app/(front-end)/page.js
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div className="">Hello</div>;
+  return (
+    <div className="flex items-center justify-center flex-col min-h-screen">
+      <h2 className="text-4xl"> Welcome to Sobji Bari</h2>
+
+      <Link className="my-4 underline" href="/register">Create an Account</Link>
+    </div>
+  );
 }
